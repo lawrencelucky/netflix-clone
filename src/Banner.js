258f649085826afdc294596317e86c08bf69f2e9
lawrencelucky <lawrencelucky1999@gmail.com>
2021-02-08 13:47:31 +0100

    Fix: Made a fix on video loading

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import movieTrailer from 'movie-trailer';
-import YouTube from 'react-youtube';
 
 import './Banner.css';
 
@@ -9,7 +7,6 @@ import requests from './requests';
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
-  const [trailerUrl, setTrailerUrl] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,27 +21,6 @@ const Banner = () => {
     };
     fetchData();
   }, []);
-
-  const handleClick = movie => {
-    if (trailerUrl) {
-      setTrailerUrl('');
-    } else {
-      movieTrailer(movie?.name || movie?.original_name || movie?.title)
-        .then(url => {
-          const urlParams = new URLSearchParams(new URL(url).search);
-          setTrailerUrl(urlParams.get('v'));
-        })
-        .catch(error => console.log(error));
-    }
-  };
-
-  const opts = {
-    height: '390',
-    width: '100%',
-    playerVars: {
-      autoplay: 1,
-    },
-  };
 
   const truncate = (str, n) =>
     str?.length > n ? str.substr(0, n - 1) + '...' : str;
@@ -65,17 +41,12 @@ const Banner = () => {
         <h1 className='banner__title'>
           {movie?.title || movie?.name || movie?.original_name}
         </h1>
-        <div className='banner__buttons'>
-          <button className='banner__button' onClick={() => handleClick(movie)}>
-            Play
-          </button>
-          <button className='banner__button'>My List</button>
-        </div>
+
         <h1 className='banner__description'>
           {truncate(movie?.overview, 150)}
         </h1>
       </div>
-      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+
       <div className='banner--fadeBottom'></div>
     </header>
   );
